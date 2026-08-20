58f8a903e3616b2c841ee74a04f45c1d578768a8
Dawid Rybacki <da.rybacki@gmail.com>
2026-08-20 18:44:56 +0200

    ESP FW starts from v0.0.1

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Firmware / protocol version
-#define DUCATI_FW_VERSION "0.1.0"
+#define DUCATI_FW_VERSION "0.0.1"
 #define DUCATI_PROTOCOL_VERSION 1
 #ifndef DUCATI_GIT_SHA
 #define DUCATI_GIT_SHA "unknown"
